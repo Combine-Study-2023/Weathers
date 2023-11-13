bfe5fee2804dadab9352661c5bdce2dec492f24d
Sejin Lee <lsj8706@naver.com>
2023-11-14 00:33:07 +0900

    Dto, 모델 생성

---
+++ Weather-Combine/Weather-Combine/Dto/WeatherDto.swift
@@ -0,0 +1,69 @@
+//
+//  WeatherDto.swift
+//  Weather-Combine
+//
+//  Created by sejin on 2023/11/14.
+//
+
+import Foundation
+
+// MARK: - Empty
+struct Empty: Codable {
+    let coord: Coord
+    let weather: [WeatherResponse]
+    let base: String
+    let main: Main
+    let visibility: Int
+    let wind: Wind
+    let clouds: Clouds
+    let dt: Int
+    let sys: Sys
+    let timezone, id: Int
+    let name: String
+    let cod: Int
+}
+
+// MARK: - Clouds
+struct Clouds: Codable {
+    let all: Int
+}
+
+// MARK: - Coord
+struct Coord: Codable {
+    let lon, lat: Int
+}
+
+// MARK: - Main
+struct Main: Codable {
+    let temp, feelsLike, tempMin, tempMax: Double
+    let pressure, humidity, seaLevel, grndLevel: Int
+
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+        case pressure, humidity
+        case seaLevel = "sea_level"
+        case grndLevel = "grnd_level"
+    }
+}
+
+// MARK: - Sys
+struct Sys: Codable {
+    let country: String
+    let sunrise, sunset: Int
+}
+
+// MARK: - Weather
+struct WeatherResponse: Codable {
+    let id: Int
+    let main, description, icon: String
+}
+
+// MARK: - Wind
+struct Wind: Codable {
+    let speed: Double
+    let deg: Int
+    let gust: Double
+}
---
+++ Weather-Combine/Weather-Combine/Model/Weather.swift
@@ -0,0 +1,14 @@
+//
+//  Weather.swift
+//  Weather-Combine
+//
+//  Created by sejin on 2023/11/14.
+//
+
+import Foundation
+
+struct Weather {
+    let title: String
+    let description: String
+    let temp: Double
+}
